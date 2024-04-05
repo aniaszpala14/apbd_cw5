e3c94780867dbdcf89c0724f655e86605237b68a
s27542 <s27542@pjwstk.edu.pl>
2024-04-05 14:45:46 +0200

    added project structure

---
+++ Tutorial4/Controllers/AnimalsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tutorial4.Database;
+using Tutorial4.Models;
+
+namespace Tutorial4.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AnimalsController : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        var animals = new MockDb().Animals;   
+        return Ok();
+    }
+    
+    [HttpDelete("{id}")]
+    public IActionResult DeleteAnimal(int id)
+    {
+        var animals = new MockDb().Animals;
+        
+        return Ok();
+    }
+}
